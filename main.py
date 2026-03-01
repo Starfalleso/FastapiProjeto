0f5ccc4482b0ca87c52f20ea0b7ab9e081c7b463
Starfalleso <vagnerhj1@gmail.com>
2026-02-28 22:00:45 -0300

    post

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Helloa a"}
+    return {"message": "Hello "}
+
+@app.get("/hello")
+async def greet():
+    return {"message": "Hello"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def say_hello(name: str,age:Optional[int] = None):
+    return {"message": f"Hello {name} and you are {age} years old"}
+
+class Student(BaseModel):
+    name: str
+    age: int
+    roll: int
+@app.post("/create_student")
+def create_student(student: Student):
+    return {
+        "name": student.name,
+        "age": student.age,
+        "roll": student.roll
+    }
